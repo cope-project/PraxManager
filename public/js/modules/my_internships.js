0a5ecc8785b8d39906272d0c74bada11c68fff82
Andrei Tofan <tofan.andrei@gmail.com>
2016-03-28 01:42:15 +0300

    hide ended internships

--- public/js/modules/my_internships.js
+++ public/js/modules/my_internships.js
@@ -2,17 +2,33 @@
  * My Internships module
  */
 
-!function () {
+!function() {
 	/**
 	 * Student internships controller
 	 */
-    NSPraxManager.controller('MyInternshipsController', ['$scope', '$http', '$modal', function ($scope, $http, $modal) {
-	$scope.internships = {}
-	
-	$http.get('/api/internships/my').success(function (data, status, headers, config){
-	    $scope.internships = data
-	})
-	
+    NSPraxManager.controller('MyInternshipsController', ['$scope', '$http', '$modal', function($scope, $http, $modal) {
+		$scope.internships = {}
+
+		$http.get('/api/internships/my').success(function(data, status, headers, config) {
+			$scope.internships = data.filter(function(internship) {
+				var days = 0;
+				if (internship.CompletionLimit == 'last3days') {
+					days = 3;
+				}
+
+				if (internship.CompletionLimit == 'last7days') {
+					days = 7;
+				}
+
+				if (internship.CompletionLimit == 'last30days') {
+					days = 30;
+				}
+				
+				var internshipEnded = moment(internship.EndDate).add(days, 'days').utc().isBefore(moment().utc());
+				return internshipEnded;
+			});
+		})
+
     }]);
-}();
+} ();
 
