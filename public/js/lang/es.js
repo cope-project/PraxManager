f673b9414920fe66863dd31c0a7a435e224400b3
Andrei Tofan <tofan.andrei@gmail.com>
2016-04-04 12:20:03 +0300

    update spanish translation

--- public/js/lang/es.js
+++ public/js/lang/es.js
@@ -1,3 +1,27 @@
 loadLanguagePack({
-    // no translation for english
+    'The current window has unsaved changes.': 'La ventana actual tiene cambios sin guardar.',
+    'Are you sure you want to delete this form ?': '¿Seguro que quiere eliminar este formulario?',
+    'Are you sure you want to delete this subject ?': '¿Seguro que quiere eliminar este tema?',
+    'Are you sure you want to delete this question ?' : '¿Seguro que quiere eliminar esta pregunta?',
+    'Copy' : 'Copiar',
+    'File upload error.': 'Error de carga de fichero',
+    'Are you sure you want to delete this internship ?' : '¿Seguro que quiere eliminar esta práctica?',
+    'Are you sure you want to archive this internship ?': '¿Seguro que quiere archivar esta práctica?',
+    'Are you sure you want to remove this student ?':'¿Seguro que quiere eliminar a este estudiante ?',
+    'Are you sure you want to remove this manager ?': '¿Seguro que quiere eliminar a este tutor?',
+    'Checkin Current Day': 'Verificación del día actual',
+    'Current Day': 'Día actual',
+    'Pending': 'Pendiente',
+    'Approved': 'Aprobado',
+    'Rejected':'Rechazado',
+    'Edit Checkin':'Editar la hora de llegada',
+    'Checked in':'Registrado',
+    'Sunday': 'Domingo',
+    'Monday': 'Lunes',
+    'Tuesday': 'Martes',
+    'Wednesday': 'Miércoles',
+    'Thursday': 'Jueves',
+    'Friday': 'Viernes',
+    'Saturday': 'Sábado',
+    'Once': 'Sólo una vez'
 });
