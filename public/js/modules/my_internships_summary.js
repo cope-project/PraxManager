fea2b901ef81b9fca86afbf9ae9df0ec6df073f5
Andrei Tofan <tofan.andrei@gmail.com>
2016-07-28 21:42:44 +0300

    add final grade in student ui

--- public/js/modules/my_internships_summary.js
+++ public/js/modules/my_internships_summary.js
@@ -5,28 +5,45 @@
 	/**
 	 * Student internship summary controller
 	 */
-    NSPraxManager.controller('MyInternshipSummaryController', ['$scope', '$http', function ($scope, $http) {
-    	$scope.internship = {};
-    	$scope.timeSum = 0;
+    NSPraxManager.controller('MyInternshipSummaryController', ['$scope', '$http', '$q',function ($scope, $http, $q) {
+		$scope.internship = {};
+		$scope.timeSum = 0;
 		$scope.user = {};
 
-    	$http.get('/api/internships/'+ PraxManager_internshipId).success(function (internship) {
-    		$scope.internship = internship;
-    	});
+		var internshipDeferred = $q.defer();
+		var userDeferred = $q.defer();
 
-    	$http.get('/api/checkin/my_checkins?internshipId='+PraxManager_internshipId)
-		.success(function (data) {
-			$scope.checkins = data;
-			$scope.checkins.forEach(function (checkin) {
-			    if(checkin.Status == 'approved'){
-				$scope.timeSum += checkin.Time;
-			    }
-			});
+		$http.get('/api/internships/' + PraxManager_internshipId).success(function (internship) {
+			$scope.internship = internship;
+			internshipDeferred.resolve(internship);
 		});
-		
+
+		$http.get('/api/checkin/my_checkins?internshipId=' + PraxManager_internshipId)
+			.success(function (data) {
+				$scope.checkins = data;
+				$scope.checkins.forEach(function (checkin) {
+					if (checkin.Status == 'approved') {
+						$scope.timeSum += checkin.Time;
+					}
+				});
+			});
+
 		$http.get('/api/my').success(function (user) {
 			$scope.user = user;
-		})
+			userDeferred.resolve(user);
+		});
+
+		$q.all([internshipDeferred.promise, userDeferred.promise]).then(function () {
+			var found = $scope.internship.AssignedUsers.find(function (user) {
+				return user._id === $scope.user._id;
+			});
+
+			if (!found) {
+				return;
+			}
+
+			$scope.finalGrade = found.finalGrade;
+		});
 
     }]);
-}();
+} ();
